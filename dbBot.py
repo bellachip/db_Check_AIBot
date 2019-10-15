47942a08701529cbc449cd6a6701f5098d5e17c5
bellachip <hwy4012@gmail.com>
2019-10-15 14:01:11 -0400

    edited for url2

--- dbBot.py
+++ dbBot.py
@@ -17,7 +17,7 @@ ex = openpyxl.load_workbook(ex_name)
 sheet = ex["Sheet1"]
 
 
-# getname
+# get name of one row
 def get_name():
     for i in range(3):
         if i >= 2:
@@ -29,69 +29,78 @@ def get_name():
             return ex_last, ex_first
 
 
-# # returns last name array and first name array
+# returns last name  and first name of first row
 l, f = get_name()
-# opes the page
-store_urls = ['https://exclusions.oig.hhs.gov/', 'https://ori.hhs.gov/case_summary']
+
+# will iterate through these urls
+# store_urls = ['https://exclusions.oig.hhs.gov/', 'https://ori.hhs.gov/case_summary']
+
+r_url = 'https://exclusions.oig.hhs.gov/'
 
 
 # does the job of clearnace checking for the first url
-def clearance_check(url, last_name_search, first_name_search):
-    if url == 'https://exclusions.oig.hhs.gov/':
-        driver.get(url)
-        print(driver.current_url)
-        # finds the sesarch bars for last name and first name
-        last_name = driver.find_element_by_id('ctl00_cpExclusions_txtSPLastName')
-        first_name = driver.find_element_by_id('ctl00_cpExclusions_txtSPFirstName')
-        last_name.send_keys(last_name_search)
-        first_name.send_keys(first_name_search)
-        last_name.send_keys(Keys.ENTER)
-        # first_name.send_keys(Keys.ENTER)
+def clearance_check(url=r_url, last_name_search=l, first_name_search=f):
+    # opening url and do the search
+    driver.get(url)
+    print(driver.current_url)
+    # finds the sesarch bars for last name and first name
+    last_name = driver.find_element_by_id('ctl00_cpExclusions_txtSPLastName')
+    first_name = driver.find_element_by_id('ctl00_cpExclusions_txtSPFirstName')
+    last_name.send_keys(last_name_search)
+    first_name.send_keys(first_name_search)
+    last_name.send_keys(Keys.ENTER)
+    # first_name.send_keys(Keys.ENTER)
 
-        # opening up connection, grabing the page
-        # uClient = uReq(driver.page_source)
-        page_html = driver.page_source
-        # uClient.close()
+    # opening up connection, grabbing the search results page
+    page_html = driver.page_source
 
-        # lxml parsing
-        page_soup = soup(page_html, "lxml")
+    # lxml parsing through the current results page
+    page_soup = soup(page_html, "lxml")
 
-        # gets the name
-        # Name = page_soup.find("div", {"id": "ctl00_cpExclusions_pnlEmpty"}).ul.li.text
-        # gets the no results value
-        if page_soup.findAll(text="No Results"):
-            # no_results = page_soup.find("div", {"id": "ctl00_cpExclusions_pnlEmpty"}).div.text
-            no_results = "No Results"
-        else:
+    # Name = page_soup.find("div", {"id": "ctl00_cpExclusions_pnlEmpty"}).ul.li.text
 
-            rows = page_soup.find("table", {"class": "leie_search_results"}).find("tbody").findAll("tr")
-            i = 1
-            for row in rows:
-                cols = row.findAll("td")
+    # gets the no results text
+    # if "No Results" is in the page then return no results variable
+    noe = page_soup.find("div", {"id": "ctl00_cpExclusions_pnlEmpty"})
 
-                for col in cols:
-                    cell = col.text
+    if noe is not None:
+        no_results = "No Results"
+    else:  # else then that means there is results for the person so scrape the info of the person
+        # gets the rows
+        rows = page_soup.find("table", {"class": "leie_search_results"}).find("tbody").findAll("tr")
 
-            if cell.find(last_name_search.upper()):
-                no_results = "Yes"
-        print(no_results)
-        return no_results
-    elif url == 'https://ori.hhs.gov/case_summary':
-        # opening up connection, grabing the page
-        u_client = uReq(url)
-        page_html = u_client.read()
-        u_client.close()
+        i = 1
 
-        # html parsing
-        page_soup = soup(page_html, "html.parser")
-        table_rows = page_soup.find("div", {"class": "view view-case-summary view-id-case_summary view-display-id-page_1 js-view-dom-id-7529d8e4a9199a663e90df04a49d98be2a9f68a770b94b8a31d23f716b4e354f ui-accordion ui-widget ui-helper-reset"}).findAll("div", {"class": "views-content"})
+        # iterate through each row
+        for row in rows:
+            cols = row.findAll("td")
+            for col in cols:
+                cell = col.text
+        if cell.find(last_name_search.upper()):
+            no_results = "Yes"
+    return no_results
 
-        for table_row in table_rows:
-            year = table_row.find()
+    # return no_results
+    # elif url == 'https://ori.hhs.gov/case_summary':
+    #     # opening up connection, grabing the page
+    #     u_client = uReq(url)
+    #     page_html = u_client.read()
+    #     u_client.close()
+    #
+    #     # html parsing
+    #     page_soup = soup(page_html, "html.parser")
+    #     years = page_soup.find_all("h3")
+    #
+    #     table_rows = page_soup.findAll("div", {"class": "views-field views-field-title"})
+    #     for table_row in table_rows:
+    #         names = table_row.a.text
+    #
+    #         if names.find(first_name_search):
+    #             no_results = "Yes"
 
 
 # checks if the person exists in the gov data base.
-def clr_check(c_check):
+def clr_check(c_check=clearance_check(r_url, l, f)):
     # for word in c_check.split():
     c2 = sheet['C2']  # should switch to the row number
     print(c_check)
@@ -104,9 +113,7 @@ def clr_check(c_check):
     return c2.value
 
 
-for store_url in store_urls:
-    clearance_check(store_url, l, f)
-    clr_check(clearance_check(store_url, l, f))
+clr_check()
 
 ex.save('db_check.xlsx')
 driver.close()
